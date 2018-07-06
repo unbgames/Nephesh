16249efbbfb36c008300aa80395ab88c081c20aa
Unknown <140039678@aluno.unb.br>
2018-07-06 00:43:36 -0300

    Started charge component

---
+++ src/Charge.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ftfnunes on 06/07/18.
+//
+
+#include <Game.h>
+#include "Charge.h"
+
+Charge::Charge(GameObject &associated, GameObject *next, float duration) : Component(associated), next(next), chargeTimer() {
+}
+
+void Charge::Update(float dt) {
+    chargeTimer.Update(dt);
+    if (chargeTimer.Get() > duration) {
+        associated.RequestDelete();
+        Game::GetInstance().GetCurrentState().AddObject(next);
+    }
+}
+
+void Charge::Render() {
+
+}
+
+bool Charge::Is(string type) {
+    return type == CHARGE_TYPE;
+}
