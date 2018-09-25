1eef72e44e6b5f726d641d136d41daa6f984ee25
Gabriel Barbosa <bestknighter@gmail.com>
2018-09-25 19:47:38 -0300

    corrige erro de CameraShaker nao encontrado

--- game/src/Log.cpp
+++ game/src/Log.cpp
@@ -5,13 +5,10 @@
 using namespace std;
 
 void Log::LogMessage(string message) {
-    ofstream file;
     time_t nowSec = time(nullptr);
     char formattedTime[500];
     strftime(formattedTime, TIMESTAMP_SIZE, TIMESTAMP_FORMAT, localtime(&nowSec));
-    file.open("log.txt", ios::app);
-    file << formattedTime << message << endl;
-    file.close();
+    cout << formattedTime << message << endl;
 }
 
 void Log::LogError(string message) {
